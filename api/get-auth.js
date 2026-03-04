0001efc667f8c4d11cdebeeacd9b2331bbde5309
myenji <enjisecond559@gmail.com>
2026-03-05 02:20:40 +0700

    Change error message for auth token fetch failure

--- api/get-auth.js
+++ api/get-auth.js
@@ -14,6 +14,6 @@ export default async function handler(req, res) {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.status(200).json(data);
     } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch websocket token' });
+        res.status(500).json({ error: 'Failed to fetch auth' });
     }
 }
